2e6379c61b38cc71b6cb0c6f4e1885dace202379
Carlos Adriano R. Cavalcante <103460781+CarlosARCN@users.noreply.github.com>
2026-04-03 19:42:29 -0300

    LogoutPage.java

---
+++ src/test/java/pages/LogoutPage.java
@@ -0,0 +1,39 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import util.DriverFactory;
+
+public class LogoutPage {
+
+    WebDriver driver;
+    By campoUsuario = By.id("user-name");
+    By campoSenha = By.id("password");
+    By botaoLogin = By.id("login-button");
+    By menuBurguer = By.id("react-burger-menu-btn");
+    By botaoLogout = By.id("logout_sidebar_link");
+
+    public LogoutPage() {
+        driver = DriverFactory.getDriver();
+    }
+
+    public void fazerLogin() throws InterruptedException {
+        driver.get("https://www.saucedemo.com");
+        Thread.sleep(2000);
+        driver.findElement(campoUsuario).sendKeys("standard_user");
+        driver.findElement(campoSenha).sendKeys("secret_sauce");
+        driver.findElement(botaoLogin).click();
+        Thread.sleep(2000);
+    }
+
+    public void realizarLogout() throws InterruptedException {
+        driver.findElement(menuBurguer).click();
+        Thread.sleep(2000);
+        driver.findElement(botaoLogout).click();
+        Thread.sleep(2000);
+    }
+
+    public String obterUrlAtual() {
+        return driver.getCurrentUrl();
+    }
+}
