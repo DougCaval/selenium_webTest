a4b6037012ba10ac61c36969b8e0fbcf606cc03b
Alexandre <alexandrepm910@gmail.com>
2026-07-27 19:21:02 -0300

    selenium_Testes

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,9 +1,9 @@
 package pages;
 
 import org.apache.commons.io.FileUtils;
-import org.junit.Assert;
 import org.openqa.selenium.*;
 import util.DriverFactory;
+import org.junit.Assert;
 
 import java.io.File;
 
@@ -16,12 +16,14 @@ public class LoginPage {
 
     By adicionarProduto = By.id("add-to-cart-sauce-labs-backpack");
     By validarCarrinho = By.id("shopping_cart_container");
-    By realizarChekcout = By.id("checkout");
+    By tituloPaginaOverview = By.className("title");
+    By realizarCheckout = By.id("checkout");
     By botaoContinue = By.id("continue");
-    By firts_name = By.id("first-name");
-    By last_name = By.id("last-name");
-    By postal_code = By.id("postal-code");
+    By campoFirstName = By.id("first-name");
+    By campoLastName = By.id("last-name");
+    By campoPostalCode = By.id("postal-code");
     By botaoFinish = By.id("finish");
+    By botaoCancel = By.id("cancel");
 
 
     WebDriver driver;
@@ -35,8 +37,7 @@ public class LoginPage {
     public void realizarLogin() throws InterruptedException {
 
         driver.get("https://www.saucedemo.com");
-        Thread.sleep(5000);
-        System.out.println("Abriu site Sauce Demo");
+        Thread.sleep(3000);
     }
 
 
@@ -56,58 +57,70 @@ public class LoginPage {
     }
 
     public void inserirDados() throws InterruptedException {
-        Thread.sleep(5000);
+        Thread.sleep(1000);
         driver.findElement(campoUsuario).sendKeys("standard_user");
-        Thread.sleep(5000);
+        Thread.sleep(1000);
         driver.findElement(campoSenha).sendKeys("secret_sauce");
-        Thread.sleep(5000);
+        Thread.sleep(1000);
         driver.findElement(botaoLogin).click();
-        Thread.sleep(5000);
+        Thread.sleep(2000);
 
     }
 
     public void adicionarProduto() throws InterruptedException {
         driver.findElement(adicionarProduto).click();
-        Thread.sleep(5000);
+        Thread.sleep(2000);
 
     }
 
     public void validarCarrinho() throws InterruptedException {
         driver.findElement(validarCarrinho).click();
-        Thread.sleep(5000);
+        Thread.sleep(2000);
     }
 
     public void realizarCheckout() throws InterruptedException {
-        driver.findElement(realizarChekcout).click();
-        Thread.sleep(5000);
+        driver.findElement(realizarCheckout).click();
+        Thread.sleep(2000);
     }
 
     public void botaoContinue() throws InterruptedException {
         driver.findElement(botaoContinue).click();
-        Thread.sleep(5000);
+        Thread.sleep(2000);
     }
 
-    private By mensagemErro = By.cssSelector("h3[data-test='error']");
+    private By mensagemErro = By.cssSelector("h3[data-test='error']"); 
 
     public String obterTextoMensagemErro() {
         return driver.findElement(mensagemErro).getText();
-
     }
 
-    public void inserirDadosCompras() throws InterruptedException {
-        Thread.sleep(5000);
-        driver.findElement(firts_name).sendKeys("Joao Lucas");
-        Thread.sleep(5000);
-        driver.findElement(last_name).sendKeys("Gonzaga");
-        Thread.sleep(5000);
-        driver.findElement(postal_code).sendKeys("38414102");
-        Thread.sleep(5000);
-
+    public void preencherDadosCheckout() throws InterruptedException {
+        driver.findElement(campoFirstName).sendKeys("Alexandre");
+        Thread.sleep(1000);
+        driver.findElement(campoLastName).sendKeys("Martins");
+        Thread.sleep(1000);
+        driver.findElement(campoPostalCode).sendKeys("38405112");
+        Thread.sleep(2000);
     }
 
     public void cliqueEmFinish() throws InterruptedException {
         driver.findElement(botaoFinish).click();
-        Thread.sleep(5000);
+        Thread.sleep(2000);
+    }
+
+    public void lerEValidarOverview() {
+        String titulo = driver.findElement(tituloPaginaOverview).getText();
+        System.out.println("Título: " + titulo);
+        Assert.assertEquals("Checkout: Overview", titulo);
+    }
+
+    public void lerEValidarCarrinho() {
+        String titulo = driver.findElement(tituloPaginaOverview).getText();
+        System.out.println("Título: " + titulo);
+        Assert.assertEquals("Your Cart", titulo);
+    }
+
+    public void clicarBotaoCancel() {
+        driver.findElement(botaoCancel).click();
     }
 }
-
--- src/test/resources/features/checkout.feature
+++ src/test/resources/features/checkout.feature
@@ -1,24 +1,18 @@
 #language: pt
 
 Funcionalidade: Checkout.
-        
-        Cenário: Voltar do overview para carrinho – botão “Cancel” ou “Back to cart”.
-                Dado que estou na página de checkout
-                Quando eu clicar no botão "Cancel"
-                Então devo ser redirecionado para a página do carrinho.
 
-        Cenário: Checkout com usuário problemático – possíveis erros de imagem ou
-cálculo.
-                Dado que estou logado com o usuário com problemas visuais
-                E houver produtos no carrinho
-                E eu estiver na página do carrinho
-                Quando eu clicar no botão de prosseguir para página de checkout
-                Então os ícones visuais devem estar nos lugares programados.
+        Cenário: Checkout com dados válidos
+                Dado que abro o navegador
+                E que o usuario possui ao menos um produto no carrinho
+                E esta na pagina de checkout 
+                Quando preencher os dados de checkout
+                E clicar em continue
+                Então deve ser direcionado para a pagina de overview do pedido
 
-        Cenário: Verificar se ao voltar sem finalizar a compra, os produtos que estavam no
-carrinho permanecem da mesma forma.
-                Dado que estou na página de checkout
-                Quando eu clicar no botão "Cancel"
-                Então devo ser redirecionado para a página do carrinho
-                E devem haver os mesmos produtos que selecionei.
-
+        Cenário: Cancelar checkout
+                Dado que abre o navegador
+                E que o usuario possui pelo menos um produto no carrinho
+                E esteja na pagina de checkout 
+                Quando clicar no botão Cancel 
+                Então deve ser direcionado para a pagina do carrinho
